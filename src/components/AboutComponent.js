47c7a1488ed1e0d7e9c48acebf0a56a88ccc95f7
grv96105 <grv96105@gmail.com>
2020-08-03 16:29:48 +0530

    Final Project

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
+import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
-
 function RenderLeader( {leader} ) {
-    return (
-      <li className="media">
-         <img className="mr-4" src={leader.image} alt="Shot" />
-         <div className="media-body">
-            <h5 className="mt-0 mb-1">{leader.name}</h5>
-            <p>{leader.designation}</p>
-            <p className="mb-4">{leader.description}</p>
-         </div>
-      </li>
-    );
-  }
+  return (
+    <li className="media">
+       <img className="mr-4" src={leader.image} alt="Shot" />
+       <div className="media-body">
+          <h5 className="mt-0 mb-1">{leader.name}</h5>
+          <p>{leader.designation}</p>
+          <p className="mb-4">{leader.description}</p>
+       </div>
+    </li>
+  );
+}
 
-function About( props ) {
+function About(props) {
 
     const leaders = props.leaders.map((leader) => {
         return (
-            <RenderLeader leader={leader}/>
+            <RenderLeader leader={leader} />
         );
     });
 
@@ -34,7 +33,7 @@ function About( props ) {
                 <div className="col-12">
                     <h3>About Us</h3>
                     <hr />
-                </div>                
+                </div>
             </div>
             <div className="row row-content">
                 <div className="col-12 col-md-6">
@@ -79,13 +78,13 @@ function About( props ) {
                     <h2>Corporate Leadership</h2>
                 </div>
                 <div className="col-12">
-                    <Media list>
+                    <ul className="list-unstyled">
                         {leaders}
-                    </Media>
+                    </ul>
                 </div>
             </div>
         </div>
     );
 }
 
-export default About;    
+export default About;
